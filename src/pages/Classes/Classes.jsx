2154821a2b01d4948ad8eb9a8dee750a06494618
Shovan Banik <53087831+Shovan-Banik@users.noreply.github.com>
2023-06-10 20:29:10 +0600

    selected class create data stored

--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -1,8 +1,17 @@
 
 import { useEffect, useState } from 'react';
+import useUserData from '../../hooks/useUserData';
+import useAuth from '../../hooks/useAuth';
+import Swal from 'sweetalert2';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Classes = () => {
     const [classes, setClasses] = useState([]);
+    const navigate=useNavigate();
+    const location=useLocation();
+    const{user}=useAuth();
+    const[userFromDB]=useUserData();
+    const role=userFromDB?.role;
 
     useEffect(() => {
         fetch('http://localhost:5000/allClasses')
@@ -14,6 +23,47 @@ const Classes = () => {
                 console.error('Error fetching classes:', error);
             });
     }, []);
+
+    const handleAddToCart=classItem=>{
+        const{_id,className,classImage,price,instructorEmail,seats}=classItem;
+        if(user && user.email){
+            const selectedClass={selectedClassId:_id,className,classImage,price,instructorEmail,seats,email:user.email}
+            fetch('http://localhost:5000/carts', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(selectedClass)
+            }).then(res => res.json())
+            .then(data => {
+                if(data.insertedId){
+                    // refetch cart to update the number of items in the cart
+                    // refetch(); 
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'class added on the cart.',
+                        showConfirmButton: false,
+                        timer: 1500
+                      })
+                }
+            })
+        }
+        else{
+            Swal.fire({
+                title: 'Please login to select the class',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Login now!'
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  navigate('/login', {state: {from: location}})
+                }
+              })
+        }
+    }
 
     return (
         <div className="my-8">
@@ -32,7 +82,7 @@ const Classes = () => {
                             <p className="mb-4">Available Seats: {classItem.seats}</p>
                             <p className="mb-4">Price: ${classItem.price}</p>
                             <div className="card-actions justify-start">
-                                <button className="btn btn-sm btn-neutral">Select</button>
+                                <button onClick={()=>handleAddToCart(classItem)} className="btn btn-sm btn-neutral" disabled={role==='admin' || role==='instructor'}>Select</button>
                             </div>
                         </div>
                     </div>
